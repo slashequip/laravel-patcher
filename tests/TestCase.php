856f234adb013bad2a890d0517243ee6684160ad
slashequip <2316916+slashequip@users.noreply.github.com>
2025-04-26 21:36:25 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,7 @@
 
 namespace SlashEquip\Patcher\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use SlashEquip\Patcher\PatcherServiceProvider;
 
 class TestCase extends Orchestra
 {
